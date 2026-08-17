4bc85bdbc3c44d0cfd1b3dab4c65150e27733fd1
raxedus <148214347+raxedus@users.noreply.github.com>
2026-08-17 23:14:31 +0300

    added almost working login page

---
+++ public/login.js
@@ -0,0 +1,11 @@
+document.getElementById("log_in_button").addEventListener('click', () => {
+    const user = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
+    const errorElement = document.getElementById('error_log_in');
+    
+    if (!user || !pass) {
+        errorElement.textContent = "Please enter both username and password.";
+    } else {
+        errorElement.textContent = ""; 
+    }
+});
