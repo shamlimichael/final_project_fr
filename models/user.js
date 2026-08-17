728d42f7e85bb0881328f324b1735d9044a6e0bc
Michael Shamli <shamli.michael888@gmail.com>
2026-08-17 23:23:04 +0300

    fixing gitignore

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    },
+    inventory: {
+        type
+    }
+})
