de89f27d57e069a5bf161c83b3104c9d48d0636e
raxedus <148214347+raxedus@users.noreply.github.com>
2026-08-17 13:32:48 +0300

    added public folder with login js and css

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
+
 app.set('view engine', 'ejs') 
 
 app.listen(3000)
 
 app.get('/', (req, res) => {
-    res.render('Login.ejs')
+    res.render('login.ejs')
 })
